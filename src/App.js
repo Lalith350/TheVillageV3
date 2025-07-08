4bf670663cf795179f67254fb060a7ba1761b0c4
Lalith350 <lalithdino@gmail.com>
2025-07-08 17:17:52 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to The Village</h1>
+      <p>This is a simple React app deployed via GitHub & Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
